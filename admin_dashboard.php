e4e02029ae433f6609b9739f3e1e2f24c5ff65a1
calinac <39138278+calinac@users.noreply.github.com>
2018-05-13 18:01:11 +0300

    added admin_dashboard.php and create_info.php file

---
+++ admin_dashboard.php
@@ -0,0 +1,52 @@
+﻿<?php require_once "includes/functions.php"; ?>
+<?php require_once "includes/connect.php"; ?>
+<?php
+$title = "Administrator dashboard";
+include("includes/simple_header.php");
+?>
+<div id = "add_form">
+	<h2>Add info</h2>
+	<form action="create_info.php" method="post">
+	<p>Titlu:
+		<input type="text" name="adminForm" value="" id="adminForm" />
+	</p>
+	<p>
+		<input type="date" name="date">
+	</p>
+	<p>Categorie:
+		<select name ="category"> 
+		<?php
+			$result = get_info_from_table('categories');
+			while ($row = mysqli_fetch_assoc($result)) {
+				echo "<option value=\"{$row['id']}\">{$row['menu']}</option>";
+				}
+		?>
+		</select>
+	</p>
+	<p>Conținut:
+	 	<textarea name="content" rows="10" cols="30">Adaugă conținut</textarea> 
+	</p>
+	<p>Vizibilitate:
+		<input type="radio" name="visible" value="0"/>No &nbsp;
+		<input type="radio" name="visible" value="1"/>Yes
+	</p>
+	<input type="submit" value="Add info"/>
+	</form>
+	<a href="index.php">Cancel</a>
+ </div>
+ <div>
+ <?php
+ $test = get_info_from_table('posts');
+	while ($row = mysqli_fetch_assoc($test)) {
+				echo $row['content'];
+				echo "<br/>";
+		}
+ ?>
+ </div>
+ </body>
+ </html>
+
+<?php
+ 	// 5. close connection  
+	require_once("includes/close_connection.php"); 
+?>
---
+++ create_info.php
@@ -0,0 +1,47 @@
+﻿<?php 
+
+require 'includes/functions.php';
+include "includes/connect.php";
+
+?>
+
+<?php
+	$title = $_POST['adminForm']; 
+	$category = trim($_POST['category']);
+	//$categories_set = get_info_from_table('categories');
+	
+	/*while ($rows = mysqli_fetch_assoc($categories_set)){
+		if(strcmp($category, $rows['menu']) == 0) {$category=$rows['id']; break;}
+		else $category=rows['id'];
+	}
+	*/
+	$date = $_POST['date'];
+	$content = $_POST['content'];
+	$visible = $_POST['visible'];
+
+	$result = get_info_from_table('posts');
+	$position = mysqli_num_rows($result) + 1; //to-add: derive position from date
+?>
+
+
+<?php
+
+	$query = "INSERT INTO posts (
+				id_categories, title, date, content, position, visible
+				) VALUES (
+				'{$category}', '{$title}', '{$date}', '{$content}', '{$position}', '{visible}'
+				)";
+
+	if(mysqli_query($conn, $query)) {
+		header("location: admin_dashboard.php");
+		exit;
+	} else {
+		echo "<p>Info create failed</p>";
+
+	}
+
+?>
+
+<?php
+	require_once "includes/close_connection.php"; 
+?>
