44bbf98d7ec016ad9620a6ed5aad5fdac9c6a5be
calinac <39138278+calinac@users.noreply.github.com>
2018-06-02 20:34:17 +0300

    add dropdown options for campus

--- includes/header.php
+++ includes/header.php
@@ -39,7 +39,13 @@
 					<a href="diverse.php">Diverse</a>
 				</div>
 			</li>
-			<li class="text-menu"><a href="complex.php">Campus</a></li>
+			<li class="dropdown"><a href="campus.php" class="text-menu">Campus</a>
+				<div class="dropdown-content">
+					<a href="javascript:void(0)" class="empty"></a>
+					<a href="universitati.php">Universități</a>
+					<a href="complex.php">Complex</a>
+				</div>
+			</li>
 			<li class="text-menu"><a href="blog.php">Blog</a></li>
 			<li class="text-menu"><a href="https://www.youtube.com/watch?v=zhW78s84xuI">Video</a></li>
 			<li class="text-menu"><a href="" onclick="document.getElementById('login').style.display='block'; return false;">Login</a></li>
