e3f1bf590f66678100e8c8cf9588169039f69eeb
calinac <39138278+calinac@users.noreply.github.com>
2018-06-04 01:10:49 +0300

    Add files via upload

---
+++ universitati.php
@@ -0,0 +1,44 @@
+﻿<?php
+	$title = "Universități";
+	include("includes/header.php");
+?>	
+<div class="universitati">
+<table id="uniTable">
+<tr>
+<td>
+<div><img src="" alt=""></div>
+</td>
+<td>
+<div id="mapUVT" style="height:90%; width:90%;"></div>
+</td>
+</tr>
+<tr>
+<td>
+<div id="mapUPT" style="height:90%; width:90%;"></div>
+</td>
+<td>
+<div><img src="" alt=""></div>
+</td>
+</tr>
+<tr>
+<td>
+<div><img src="" alt=""></div>
+</td>
+<td>
+<div id="mapUSAMVB" style="height:90%; width:90%;"></div>
+</td>
+</tr>
+<tr>
+<td>
+<div id="mapUMFT" style="height:90%; width:90%;"></div>
+</td>
+<td>
+<div><img src="" alt=""></div>
+</td>
+</tr>
+</table>
+
+<?php include 'map_include_uni.php'; ?>
+
+<?php include_once "includes/footer.php"; ?>
+
