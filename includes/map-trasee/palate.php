d165d2a2f2afa9949d17d4ad1f318fd0209f4959
calinac <39138278+calinac@users.noreply.github.com>
2018-06-04 01:11:54 +0300

    Add files via upload

---
+++ includes/map-trasee/palate.php
@@ -0,0 +1,45 @@
+	<strong>Plecare:</strong>
+    <select id="start">
+		<option value="{lat: 45.7574831 , lng: 21.2288064 }">﻿Palatul Bruck</option>
+		<option value="{lat: 45.7570104 , lng: 21.2313452 }">Palatul Dicasterial</option>
+		<option value="{lat: 45.7536332 , lng: 21.2250684 }">Palatul Lloyd</option>
+		<option value="{lat: 45.7492406 , lng: 21.2195174 }">Palatul Apelor</option>
+		<option value="{lat: 45.7560435 , lng: 21.231342 }">Palatul Dejan</option>
+		<option value="{lat: 45.7526209 , lng: 21.2246531 }">Palatul Dauerbach</option>
+		<option value="{lat: 45.7540346 , lng: 21.2249458 }">Palatul Weiss</option>
+		<option value="{lat: 45.75166 , lng: 21.2240217 }">Palatul Széchenyi</option>
+		<option value="{lat: 45.7519768 , lng: 21.2243829 }">Palatul Hilt-Vogel</option>
+		<option value="{lat: 45.7531814 , lng: 21.2259519 }">Palatul Löffler</option>
+		<option value="{lat: 45.7531066 , lng: 21.2247427 }">Palatul Merbl</option>
+		<option value="{lat: 45.7532844 , lng: 21.2248233 }">Palatul Neuhausz</option>
+		<option value="{lat: 45.7565464 , lng: 21.2419581 }">Palatul Neptun</option>
+		<option value="{lat: 45.7580596 , lng: 21.2495621 }">Palatul Mercur</option>
+		<option value="{lat: 45.7559607 , lng: 21.23179 }">Casa Mercy</option>
+		<option value="{lat: 45.7488716 , lng: 21.2086793 }">Palatul Bancii Nationale</option>
+		<option value="{lat: 45.7571013 , lng: 21.2287297 }">Palatul Emmer</option>
+		<option value="{lat: 45.7491241 , lng: 21.220139 }">Palatul Marschall</option>
+		<option value="{lat: 45.7471195 , lng: 21.210202 }">Palatul Ancorei</option>
+	</select>
+    <br>
+    <strong>Destinație:</strong>
+    <select id="end">
+		<option value="{lat: 45.7574831 , lng: 21.2288064 }">﻿Palatul Bruck</option>
+		<option value="{lat: 45.7570104 , lng: 21.2313452 }">Palatul Dicasterial</option>
+		<option value="{lat: 45.7536332 , lng: 21.2250684 }">Palatul Lloyd</option>
+		<option value="{lat: 45.7492406 , lng: 21.2195174 }">Palatul Apelor</option>
+		<option value="{lat: 45.7560435 , lng: 21.231342 }">Palatul Dejan</option>
+		<option value="{lat: 45.7526209 , lng: 21.2246531 }">Palatul Dauerbach</option>
+		<option value="{lat: 45.7540346 , lng: 21.2249458 }">Palatul Weiss</option>
+		<option value="{lat: 45.75166 , lng: 21.2240217 }">Palatul Széchenyi</option>
+		<option value="{lat: 45.7519768 , lng: 21.2243829 }">Palatul Hilt-Vogel</option>
+		<option value="{lat: 45.7531814 , lng: 21.2259519 }">Palatul Löffler</option>
+		<option value="{lat: 45.7531066 , lng: 21.2247427 }">Palatul Merbl</option>
+		<option value="{lat: 45.7532844 , lng: 21.2248233 }">Palatul Neuhausz</option>
+		<option value="{lat: 45.7565464 , lng: 21.2419581 }">Palatul Neptun</option>
+		<option value="{lat: 45.7580596 , lng: 21.2495621 }">Palatul Mercur</option>
+		<option value="{lat: 45.7559607 , lng: 21.23179 }">Casa Mercy</option>
+		<option value="{lat: 45.7488716 , lng: 21.2086793 }">Palatul Bancii Nationale</option>
+		<option value="{lat: 45.7571013 , lng: 21.2287297 }">Palatul Emmer</option>
+		<option value="{lat: 45.7491241 , lng: 21.220139 }">Palatul Marschall</option>
+		<option value="{lat: 45.7471195 , lng: 21.210202 }">Palatul Ancorei</option>
+    </select>
---
+++ includes/map-trasee/poduri.php
@@ -0,0 +1,31 @@
+	<strong>Plecare:</strong>
+    <select id="start">
+		<option value="{lat: 45.7580631 , lng: 21.26516 }">Pasarela de la Uzina Hidroelectrică</option>
+		<option value="{lat: 45.7486592 , lng: 21.2254587 }">Podul Andrei Șaguna</option>
+		<option value="{lat: 45.7560746 , lng: 21.2409611 }">Podul Decebal</option>
+		<option value="{lat: 45.7497206 , lng: 21.2209141 }">Podul Traian</option>
+		<option value="{lat: 45.7488716 , lng: 21.2086793 }">Pasarela îndrăgostiților</option>
+		<option value="{lat: 45.7503479 , lng: 21.2346531 }">Podul Michelangelo</option>
+		<option value="{lat: 45.7599588 , lng: 21.258903 }">Podul Mihai Viteazu</option>
+		<option value="{lat: 45.7490636 , lng: 21.2145765 }">Podul Metalic</option>
+		<option value="{lat: 45.7466161 , lng: 21.2103327 }">Podul General Ioan Dragalina</option>
+		<option value="{lat: 45.7466246 , lng: 21.2074133 }">Podul Eroilor</option>
+		<option value="{lat: 45.7458716 , lng: 21.205131 }">Podul Iuliu Maniu</option>
+		<option value="{lat: 45.7440749 , lng: 21.1996889 }">Pasarela Gelu</option>
+	</select>
+    <br>
+    <strong>Destinație:</strong>
+    <select id="end">
+		<option value="{lat: 45.7580631 , lng: 21.26516 }">Pasarela de la Uzina Hidroelectrică</option>
+		<option value="{lat: 45.7486592 , lng: 21.2254587 }">Podul Andrei Șaguna</option>
+		<option value="{lat: 45.7560746 , lng: 21.2409611 }">Podul Decebal</option>
+		<option value="{lat: 45.7497206 , lng: 21.2209141 }">Podul Traian</option>
+		<option value="{lat: 45.7488716 , lng: 21.2086793 }">Pasarela îndrăgostiților</option>
+		<option value="{lat: 45.7503479 , lng: 21.2346531 }">Podul Michelangelo</option>
+		<option value="{lat: 45.7599588 , lng: 21.258903 }">Podul Mihai Viteazu</option>
+		<option value="{lat: 45.7490636 , lng: 21.2145765 }">Podul Metalic</option>
+		<option value="{lat: 45.7466161 , lng: 21.2103327 }">Podul General Ioan Dragalina</option>
+		<option value="{lat: 45.7466246 , lng: 21.2074133 }">Podul Eroilor</option>
+		<option value="{lat: 45.7458716 , lng: 21.205131 }">Podul Iuliu Maniu</option>
+		<option value="{lat: 45.7440749 , lng: 21.1996889 }">Pasarela Gelu</option>
+    </select>
